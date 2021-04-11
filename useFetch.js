433cff871ba2ddc3f3a3639466a6c70d32498abc
manuela-cepeda <manulcepeda@gmail.com>
2021-04-11 12:32:15 -0300

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from "react"
+
+
+export const useFetch = (url) => {
+   
+
+    const isMounted = useRef(true);
+    const [state, setState] = useState({data: null, loading:true, error:null});
+    
+    useEffect(() => {
+        return() => {
+            isMounted.current = false;
+        }
+    }, [])
+
+    useEffect(() => {
+        setState({data: null, loading:true, error:null} );
+       fetch(url)
+       .then (resp => resp.json())
+       .then (data => {
+
+                if(isMounted.current){
+                    setState({
+                        loading:false,
+                        error:null,
+                        data
+                    }) ;
+
+                }
+          
+            //uso el useRef  para evitar error cuando despongo componente
+            //(ejercicio real example ref)
+            //aca se vuelve util el useREf
+           
+
+       })
+       .catch (() => {
+           setState({
+               data: null,
+               loading: false,
+               error: 'No se pudo cargar la info'
+           })
+       })
+
+    }, [url]);
+
+    return state;
+}
